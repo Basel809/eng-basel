f0871c6a864dbb5e4a21b5ea2da6ca7382c08fde
Basel809 <you@domain.com>
2021-02-11 19:45:27 +0200

    eng

---
+++ BAS2.js
@@ -0,0 +1,127 @@
+
+// // this is the sentance
+// alert(" welcoome to bage basel shaheen ")
+// var num=560;
+// console.log(num)
+// var st="basel shaheen alashakhrit"
+// console.log("the name is:"+st);
+// var nm1=4;
+// var nm2=2;
+// nm1+=nm2
+// console.log(nm1 );
+// document.getElementById("p1") .outerHTML="basel shaheen alshakhrityyy";
+// document.write("<h1>baseeeeeeeeeeeeee</h1>");
+// var text ="basel shaheen (BSH)";
+// text= text.charAt(10);
+// document.getElementById("p1").innerHTML=text;
+// var text="basel m shaheen (Bsh)";
+// // text=text.length;
+//  var indexOf=text.indexOf("Html");
+// document.getElementById("p1").innerHTML=indexOf;
+// var text="Basel M Shaheen (Bsh)";
+// // text=text.length;
+// // text=text.lastIndexOf("shaheen")
+// // text=text.search("shaheen");
+// // text=text.startsWith("B");
+// text=text.endsWith("(Bsh)")
+
+// document.getElementById("p1").innerHTML=text;
+//درس جديدي اسمهreplace && substring
+// let text="basel$shaheen$is$love$the$allh$allh$allh ";
+
+// text=text.replace(/allh/i, "programing");
+// text=text.replace(/\$/g, "" ");
+//  basel=text.substring(6 ,14)
+ 
+// document.getElementById("p1").innerHTML=basel;
+//درس جديدarray
+// var id_1=["basel",20, true, "shaheen"];
+// console.log(id_1[0], id_1[2]);
+// console.log(id_1.indexOf("shaheen"));
+
+// alert(id_1.indexOf("shaheen"));
+// alert(id_1.length);
+// alert("the name :"+id_1[0]+"\n the age :"+ id_1[1]+ "\n are you marred ?:"+ id_1[2]+ "\n the name of famly :"+id_1[3])
+// alert(id_1.indexOf("basel"));
+//درس جديد بعنوانjavasecript object
+// let  data={name:"Basel" ,
+//  age:20 ,
+//   famly:"shaheen" ,
+//   mared:true ,
+//    languege:["html", "css", "javasecrbit"] ,
+//     myfunction:function(){
+//     return(10*10);
+// }
+// };
+// data.name="basel shaheen al shahkhrit"; 
+// delete data.famly;
+// data.study="it";
+// console.log(data);
+// alert(data);
+// // alert(data.name +"    \n"+data.languege[1]+"     \n"+ data.famly);
+
+// alert(data.myfunction());
+// درس جديد بعنوان object ذاخل object
+// var users ={
+//     user1:{name:"basel", email:"almanbasel @gmail.com" }
+//     ,
+//     user2:{name:"bader", email:"almanbader @gmail.com" }
+//     ,
+//     user3:{name:"deema", email:"almandeema@gmail.com" }
+// };
+
+
+// alert(users.user1.email);
+//درس جديد بعنوان if statment
+//ملاحظة(1): اذا تم وضع === معنا لازم يكونن القيميتن متساويتان ومن نفس النوع
+// var num=50;
+// if(num>=50){
+//     alert("ملاحظة"+"\nحسب علامة الطالب الطالب معدله فوق ال50 اذا ناجح");
+// }
+// else if(num>30 && num<50)
+// {
+
+//     alert ("ملاحظة"+"\nمتوسط");
+// }
+// else{
+//     alert ("ملاحظة"+"\nراسب");
+// }
+// درس جديد العوامل المنطقية
+//  var num1=true;
+//  var num2=false;
+//  if(! num1==true){
+//      alert("تم التحقق من الشرط");
+//     }
+     
+//      else{
+//          alert("لم يتم التحقق من الشرط");
+//      };
+//  درس جديد switch case
+// var date =2007;
+// switch(date){
+//     case 1990:
+//         alert("the date is : 1990");
+        
+//         break;
+//     case 1995:
+//             alert("the date is : 1995");
+//             break;       
+//     case 1999:
+//                 alert("the date is : 1999");
+//                 break;
+//     case 2000:
+//                     alert("the date is : 2000");
+//                     break;
+
+// default:
+//     alert("the chosin is not avalblity")
+
+
+
+// درس جديد بعنوان while loop
+// var cont=0;
+// while(cont<=100){
+//     document.getElementById("p1").innerHTML+="Studant"+cont+"<br>";
+//     ++cont;
+// }
+
